ebacbed857ede927aa875faa0e59767eef0b8494
vinsensiusrinda <vinsensius008@gmail.com>
2023-10-29 22:10:28 +0700

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,7 +39,7 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/search/-7.115629708806639,+110.39910070354553?sa=X&ved=2ahUKEwipvOOExZuCAxUtxjgGHXK7C4kQ8gF6BAgPEAA&ved=2ahUKEwipvOOExZuCAxUtxjgGHXK7C4kQ8gF6BAgQEAI"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
@@ -56,12 +56,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                vinsensiusrindaresi@email.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">081-21-554-556</p>
             </div>
           </div>
         </div>
