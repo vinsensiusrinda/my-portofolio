4c047568a30f633fb046f23d80dda8b20c0352c5
vinsensiusrinda <vinsensius008@gmail.com>
2023-11-01 16:20:32 +0700

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About() {
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
-                    className="object-cover object-center rounded" alt="hero" src="./coding/svg" />
+                    className="object-cover object-center rounded" alt="hero" src="./coding.svg" />
                 </div>
             </div>
         </section>
